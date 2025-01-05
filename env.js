3526056152a4332c65cab93f7872138c72e252ea
inkdrive <fractal.ink.drive@gmail.com>
2025-01-05 21:18:27 +0530

    Update env.js

--- env.js
+++ env.js
@@ -1,6 +1,3 @@
-// env.js
-const supabaseUrl = 'https://ovjtjgpirxjfnogxzwiu.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im92anRqZ3BpcnhqZm5vZ3h6d2l1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU5OTE2MjQsImV4cCI6MjA1MTU2NzYyNH0.AAc-uQw0E9-4SFTFjFUnmokJXlOoDX-7QWWHCl7oqns';
-
-// Export the supabase environment variables
-export { supabaseUrl, supabaseKey };
+//env.js
+export const SUPABASE_URL = 'https://ovjtjgpirxjfnogxzwiu.supabase.co';
+export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im92anRqZ3BpcnhqZm5vZ3h6d2l1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU5OTE2MjQsImV4cCI6MjA1MTU2NzYyNH0.AAc-uQw0E9-4SFTFjFUnmokJXlOoDX-7QWWHCl7oqns';
